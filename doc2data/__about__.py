e98ad1d437eaa66dcfdad30e93299db5442a7b0b
Sergej Levich <sergej.levich@gmail.com>
2022-08-08 11:33:51 +0200

    Update project structure

--- doc2data/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present Sergej Levich <sergej.levich@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = '0.0.1'
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present Sergej Levich <sergej.levich@gmail.com>
-#
-# SPDX-License-Identifier: MIT
