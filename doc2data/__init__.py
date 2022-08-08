dda765e6ee368c22232330b2bfec4f424fbc2c7f
Sergej Levich <sergej.levich@gmail.com>
2022-08-08 18:28:55 +0200

    Update __init__.py and version configuration

--- doc2data/__init__.py
+++ doc2data/__init__.py
@@ -1,3 +1,7 @@
 # SPDX-FileCopyrightText: 2022-present Sergej Levich <sergej.levich@gmail.com>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: Apache-2.0
+
+"""doc2data - Integrated document processing with machine learning."""
+
+__version__ = "0.0.1"
